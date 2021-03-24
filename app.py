1d5d1727f4341efd1eb8512f53a23a1702ae92a1
Paul Harrington <paulharrington05@gmail.com>
2021-03-24 22:20:21 +0000

    Add option to add new category to DB

--- app.py
+++ app.py
@@ -161,6 +161,19 @@ def manage_categories():
     return render_template('manage_categories.html', categories=categories)
 
 
+@app.route('/add_category', methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash('Category added successfully!')
+        return redirect(url_for('manage_categories'))
+
+    return render_template('add_category.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
